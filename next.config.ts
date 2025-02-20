05fceda6eb4406017312917c440d18f4e30847d1
Lacson <lucmouguengui@gmail.com>
2025-02-20 23:37:43 +0100

    Fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,10 @@ import type { NextConfig } from 'next';
 import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Mise en place de React Strict Mode au niveau de Next.js
+  reactStrictMode: true,
   pwa: {
-    dest: 'public', // Dossier où seront générés les fichiers PWA
-    disable: process.env.NODE_ENV === 'development', // Désactiver PWA en mode développement
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/your-api\.com\//, // Exemple d'API à mettre en cache
-        handler: 'NetworkFirst', // Stratégie de mise en cache
-        options: {
-          cacheName: 'api-cache',
-          expiration: {
-            maxEntries: 10, // Nombre maximum d'entrées dans le cache
-            maxAgeSeconds: 24 * 60 * 60, // Expiration après 24h
-          },
-        },
-      },
-    ],
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
   },
 };
 
